121a85076ca5e8320d7b04d65ca1f50fd014b38b
Sainath <latkarsainath@gmail.com>
2021-07-28 15:10:44 -0700

    change text

--- src/components/sections/hero.js
+++ src/components/sections/hero.js
@@ -59,14 +59,13 @@ const Hero = () => {
   }, []);
 
   const one = <h1>Hi, my name is</h1>;
-  const two = <h2 className="big-heading">Brittany Chiang.</h2>;
-  const three = <h3 className="big-heading">I build things for the web.</h3>;
+  const two = <h2 className="big-heading">Sainath Latkar.</h2>;
+  const three = <h3 className="big-heading">I am a software engineer.</h3>;
   const four = (
     <p>
-      I'm a software engineer specializing in building (and occasionally designing) exceptional
-      digital experiences. Currently, I'm focused on building accessible, human-centered products at{' '}
+      Currently, I'm studying masters in Computer Science {' '}
       <a href="https://upstatement.com/" target="_blank" rel="noreferrer">
-        Upstatement
+        Arizona State University
       </a>
       .
     </p>
