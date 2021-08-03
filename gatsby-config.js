fbe0ad5126a9207d8b694d52793f7c95edcbee9b
Sainath <latkarsainath@gmail.com>
2021-08-02 18:49:56 -0700

    Fix typo

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,12 +2,12 @@ const config = require('./src/config');
 
 module.exports = {
   siteMetadata: {
-    title: 'Brittany Chiang',
+    title: 'Sainath Latkar',
     description:
-      'Brittany Chiang is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
-    siteUrl: 'https://brittanychiang.com', // No trailing slash allowed!
+      'Sainath Latkar is a software engineer who specializes in building distributed systems.',
+    siteUrl: 'https://sainathl.com', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
-    twitterUsername: '@bchiang7',
+    twitterUsername: '@sainathl13',
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
